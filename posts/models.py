fcd6b187b6695fba94cfd0a206a63a6fab3af2a2
shuvo2k <ninjacoder2k@gmail.com>
2018-03-31 12:17:02 +0600

    comments model updated

--- posts/models.py
+++ posts/models.py
@@ -19,3 +19,19 @@ class Post(models.Model):
     def __str__(self):
         return self.message
 
+
+class Comment(models.Model): 
+    comment = models.CharField(max_length=150)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+    
+    def __str__(self):
+        return self.comment
+
+        
